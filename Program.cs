44908cab11edd4116554391de7843751c2d7be41
Richard Seroter <richardseroter@Richards-MacBook-Pro-2.local>
2017-06-16 11:33:06 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+
+namespace bootcamp_core_loadtester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing load ...");
+
+            HttpClient c = new HttpClient();
+
+            for(int i = 0; i < 2000; i++)
+            {
+                var result = c.GetStringAsync("https://c<enter your service>/api/products").Result; 
+                Console.WriteLine("calling API");
+                
+                System.Threading.Thread.Sleep(200);
+            }   
+
+            Console.WriteLine("done calling API");
+        
+        }
+    }
+}
